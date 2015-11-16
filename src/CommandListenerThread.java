67a0555b71d40fc6ebcbe315452535c047e2a5e0
ZiberEvhenii <ziberz@mail.ru>
2015-11-16 22:07:36 +0200

    Разрабатывается класс CommandListenerThread

--- src/CommandListenerThread.java
+++ src/CommandListenerThread.java
@@ -1,27 +1,34 @@
 
 public class CommandListenerThread implements Runnable {
+    private Connection connect;
+    private Command lastCom;
+    private volatile boolean isDisconnected;
     CommandListenerThread(){
-
+        connect = null;
+        start();
     }
 
     CommandListenerThread(Connection con){
-
+        this.connect = con;
+        start();
     }
 
     public void start(){
+        isDisconnected = false;
+        run();
 
     }
 
     public void stop(){
-
+        isDisconnected = true;
     }
 
     public Command getLastCommand(){
-        return null;
+        return lastCom;
     }
 
     public boolean isDisconnected(){
-        return true;
+        return isDisconnected;
     }
 
     public void run() {
