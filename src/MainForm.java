a891131a3e9ebc9f9d7d478b66ee9b9d19236505
ZiberEvhenii <ziberz@mail.ru>
2015-11-17 17:11:43 +0200

    Добавление MainForm

---
+++ src/MainForm.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class MainForm extends  JFrame {
+    private JPanel panel1;
+    private JButton disconnectButton;
+    private JButton connectButton;
+    private JTextField DAppSeTextField;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton applyButton;
+    private JButton sendButton;
+    private JTextField textField4;
+    private JScrollBar scrollBar1;
+    private JList list1;
+
+    public MainForm(){
+        setContentPane(panel1);
+        setTitle("Incoming connection");
+        setSize(400,200);
+        setVisible(true);
+    }
+    public static void main (String [] args){
+        new MainForm();
+    }
+}
