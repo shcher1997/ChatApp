631ba8467199bdf9f6da1b38dfc23074c74c36dc
shcher1997 <annnuta_2011@ukr.net>
2015-11-18 23:45:43 +0200

    new FORM!(with TextArea and ScrollBar)

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class MainForm extends  JFrame {
     private JPanel panel1;
@@ -11,14 +12,37 @@ public class MainForm extends  JFrame {
     private JButton sendButton;
     private JTextField textField4;
     private JScrollBar scrollBar1;
-    private JList list1;
+    private JTextArea textArea1;
 
     public MainForm(){
         setContentPane(panel1);
         setTitle("Incoming connection");
         setSize(700,600);
         setVisible(true);
+
+    /*    sendButton.addActionListener(ActionEvent e){
+
+
+        }*/
     }
+
+
+    /*
+    try {
+					if (!messageArea.getText().equals("")) {
+						connection.sendMessage(messageArea.getText());
+						model.addMessage(nickField.getText(), new Date(), messageArea.getText());
+
+						textArea.update(model, new Object());
+						messageArea.setText("");
+
+					}
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+     */
+
     public static void main (String [] args){
         new MainForm();
     }
